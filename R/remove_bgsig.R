7578fe558ba46555b4e3c02ccf58a886d1ae903c
xqzou <zoe.zou@astrazeneca.com>
2022-12-05 13:54:04 +0000

    add DESCRIPTION

--- R/remove_bgsig.R
+++ R/remove_bgsig.R
@@ -1,4 +1,13 @@
-# Wrap for subtracting the background from the target signal to estimate the KO-associated signature
+#' Wrap for subtracting the background to estimate the experimentally generated signature
+#' @param MutCatalogue 96-channel mutational catalog
+#' @param bg_column the column name of the background catalog
+#' @param ko_columns the column names of the experiment catalog
+#' @param sampling_number
+#' @param start_num
+#' @param boundary
+#' @param outputname
+#' @return
+#' @export
 Wrap_KOSig <- function(MutCatalogue,bg_column,ko_column,sampling_number, start_num,boundary,outputname){
   
   KOSig <- RemoveBackground_vector_single(MutCatalogue[,bg_column], MutCatalogue[,ko_column],sampling_number, start_num,boundary)
@@ -8,4 +17,105 @@ Wrap_KOSig <- function(MutCatalogue,bg_column,ko_column,sampling_number, start_n
   plotPercentagebasis(KOSig,1,6,9,paste0(outputname,"_percentage.pdf"))
   
 }
+
+
+#' Return single vector
+#' @param background_profile background mutational catalog
+#' @param sig_profile experimentally generated mutational catalog
+#' @param sampling_number number of bootstrapping samples
+#' @param start_num mutation burden to start
+#' @param boundary Range of sigature around centroid (default = 2)
+#' @return
+#' @export
+RemoveBackground_vector_single <- function(background_profile, sig_profile,sampling_number, start_num,boundary=2){
+  
+  # Remove weak mutation types in sig_profile
+  removeWeakMutationTypes <- 0.01
+  genomesOriginal <- as.data.frame(sig_profile)
+  Totalmutations <- sum(sig_profile)
+  removeMutations_max <- removeWeakMutationTypes * Totalmutations
+  removeIdx <- which(cumsum(rowSums(genomesOriginal[,1:dim(genomesOriginal)[2],drop = FALSE])[order(rowSums(genomesOriginal[,1:dim(genomesOriginal)[2],drop = FALSE]))])<= min(removeMutations_max,4))
+  mutationTypesToRemoveSet <- order(rowSums(genomesOriginal[,1:dim(genomesOriginal)[2],drop = FALSE]))[removeIdx]
+  genomesReducted <- as.data.frame(genomesOriginal[-mutationTypesToRemoveSet,])
+  reducedMutationtypes <- dim(genomesReducted)[1]
+  
+  # bootstrap sig profile
+  centroid_sig <- rowMeans(genomesReducted[,1:dim(genomesReducted)[2],drop = FALSE])
+  RepSig <- matrix(rep(centroid_sig,sampling_number),ncol = sampling_number)
+  Sig_bootstraps <- bootstrapGenomesfun2(RepSig,sum(centroid_sig))
+  
+  i=start_num
+  reachLimit <- FALSE
+  
+  while(i<sum(centroid_sig) & !reachLimit){
+    # Remove the same weak mutation types in background profile
+    backgroundReducted <- background_profile[-mutationTypesToRemoveSet]
+    RepControl <- matrix(rep(backgroundReducted,sampling_number),ncol = sampling_number)
+    bg_bootstraps <- bootstrapGenomesfun2(RepControl,i)
+    
+    # Range of background
+    centroid_background <- rowMeans(bg_bootstraps)
+    sd_background <- apply(bg_bootstraps,1,sd)
+    boundary_background <- centroid_background+boundary*sd_background
+    
+    # Range of sig
+    centroid_sig <- rowMeans(Sig_bootstraps)
+    sd_sig <- apply(Sig_bootstraps,1,sd)
+    boundary_sig <- centroid_sig+boundary*sd_sig
+    
+    
+    diff_all_boundary <- boundary_sig-centroid_background
+    diff_all <- centroid_sig-centroid_background
+    
+    if(length(which(diff_all_boundary<0))>0){
+      reachLimit <- TRUE
+    }
+    
+    if(length(which(diff_all_boundary<0))==0){
+      diff_all_boundary_save <- diff_all_boundary
+      diff_all_save <- diff_all
+      diff_all_save[which(diff_all_save<0)] <- 0
+    }
+    
+    
+    
+    i = i+1
+    
+  }
+  # Add Weak mutations for KO expoure
+  exposure <- rep(0,96)
+  origArrayIndex <- 1
+  for(i in 1:96){
+    if(! i %in% mutationTypesToRemoveSet){
+      exposure[i] <- diff_all_save[origArrayIndex]
+      origArrayIndex=origArrayIndex+1
+    }
+  }
+  
+  # Add Weak mutations for background
+  background_exposure<- rep(0,96)
+  origArrayIndex <- 1
+  for(i in 1:96){
+    if(! i %in% mutationTypesToRemoveSet){
+      background_exposure[i] <- centroid_background[origArrayIndex]
+      origArrayIndex=origArrayIndex+1
+    }
+  }
+  
+  
+  return(data.frame("KO_exposure"=exposure,"background_exposure"=background_exposure))
+  
+  
+}
+
+#' Wrap for subtracting the background to estimate the experimentally generated signature
+#' @param genomes mutational catalog for bootstrapping
+#' @param n number of bootstrapping to draw
+#' @return a matrix of bootstrapping sample catalogs
+#' @export
+bootstrapGenomesfun2 <- function(genomes,n){
+  
+  return(apply(genomes, 2, function(x) rmultinom(1, n, x)))
+}
+
 
