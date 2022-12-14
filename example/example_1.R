0ca8970ededc3772db97a8dc3ef379779a9b7bdc
xqzou <zoe.zou@astrazeneca.com>
2022-12-14 10:50:08 +0000

    updated example

--- example/example_1.R
+++ example/example_1.R
@@ -13,13 +13,13 @@ plotCountbasis(sig,1,6,9,paste0("MShigh",".pdf"))
 plotPercentagebasis(sig,1,6,9,paste0("MShigh","_percentage.pdf"))
 
 # Med
-Wrap_KOSig(df,"bg_profile",c("EMSmedRep4c.1","EMSmedRep2c.1","EMSmedRep3c.1"),100, 400,2,"EMSmed")
+Wrap_KOSig(df,"bg_profile",c("EMSmedRep4c.1","EMSmedRep2c.1","EMSmedRep3c.1"),100, bg_mean*0.8,2,"EMSmed")
 sig <- read.table("EMSmed.txt", sep = "\t", header = T, as.is = T)
 plotCountbasis(sig,1,6,9,paste0("EMSmed",".pdf"))
 plotPercentagebasis(sig,1,6,9,paste0("EMSmed","_percentage.pdf"))
 
 # Low
-Wrap_KOSig(df,"bg_profile",c("EMSlowRep2c.1","EMSlowRep3c.1","EMSlowRep4c.1"),100, 400,3,"EMSlow")
+Wrap_KOSig(df,"bg_profile",c("EMSlowRep2c.1","EMSlowRep3c.1","EMSlowRep4c.1"),100, bg_mean*0.8,3,"EMSlow")
 sig <- read.table("EMSlow.txt", sep = "\t", header = T, as.is = T)
 plotCountbasis(sig,1,6,9,paste0("EMSlow",".pdf"))
 plotPercentagebasis(sig,1,6,9,paste0("EMSlow","_percentage.pdf"))
