614c2da477e63d7207dd7e39d43a56a2f4117936
xqzou <zoe.zou@astrazeneca.com>
2022-12-14 12:08:03 +0000

    updated example

--- example/example_1.R
+++ example/example_1.R
@@ -1,25 +1,15 @@
 library("ExpSigfinder")
 library(tidyverse)
 
-df <- read.table("pig_a_mutation_matrix.tsv", sep = "\t", header = T, as.is = T) 
+df <- read.table("example.tsv", sep = "\t", header = T, as.is = T) 
 df <- df %>%
-  mutate(bg_profile=rowMeans(df[,c("VehCtrlRep3c.1","VehCtrlRep4c.1","VehCtrlRep2c.1")]))
+  mutate(bg_profile=rowMeans(df[,c("treatment.1","treatment.2","treatment.3")]))
 
+# calculate the average number of mutation burden in controls
 bg_mean <- sum(df$bg_profile)
-# High
-Wrap_KOSig(df,"bg_profile",c("EMShighRep2c.1","EMShighRep3c.1","EMShighRep4c.1"),100, bg_mean,2,"EMShigh")
-sig <- read.table("EMShigh.txt", sep = "\t", header = T, as.is = T)
-plotCountbasis(sig,1,6,9,paste0("MShigh",".pdf"))
-plotPercentagebasis(sig,1,6,9,paste0("MShigh","_percentage.pdf"))
 
-# Med
-Wrap_KOSig(df,"bg_profile",c("EMSmedRep4c.1","EMSmedRep2c.1","EMSmedRep3c.1"),100, bg_mean*0.8,2,"EMSmed")
-sig <- read.table("EMSmed.txt", sep = "\t", header = T, as.is = T)
-plotCountbasis(sig,1,6,9,paste0("EMSmed",".pdf"))
-plotPercentagebasis(sig,1,6,9,paste0("EMSmed","_percentage.pdf"))
 
-# Low
-Wrap_KOSig(df,"bg_profile",c("EMSlowRep2c.1","EMSlowRep3c.1","EMSlowRep4c.1"),100, bg_mean*0.8,3,"EMSlow")
-sig <- read.table("EMSlow.txt", sep = "\t", header = T, as.is = T)
-plotCountbasis(sig,1,6,9,paste0("EMSlow",".pdf"))
-plotPercentagebasis(sig,1,6,9,paste0("EMSlow","_percentage.pdf"))
+Wrap_KOSig(df,"bg_profile",c("treatment.1","treatment.2","treatment.3"),100, bg_mean,2,"treatment")
+sig <- read.table("treatment.txt", sep = "\t", header = T, as.is = T)
+plotCountbasis(sig,1,6,9,paste0("treatment",".pdf"))
+plotPercentagebasis(sig,1,6,9,paste0("treatment","_percentage.pdf"))
